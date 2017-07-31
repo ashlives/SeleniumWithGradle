7bfbda11b1cae5c2c64a1851aad5b77bb16a993a
Ashish <ashish.p.deshmukh@gmail.com>
2017-07-31 15:05:26 +0530

    added a groovy file for test purpose

---
+++ src/main/groovy/Example.groovy
@@ -0,0 +1,9 @@
+/**
+ * Created by Lenovo G50-70 on 31-07-2017.
+ */
+class Example {
+    static void main(String[] args){
+        // Using a simple println statement to print output to the console
+        println('Hello World')
+    }
+}
